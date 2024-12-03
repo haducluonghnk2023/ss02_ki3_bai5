3b8299d9ad6deea532167610ff4ebbcf1f63b372
haducluonghnk2023 <147390439+haducluonghnk2023@users.noreply.github.com>
2024-12-03 22:08:56 +0700

    Update bai5.c

--- bai5.c
+++ bai5.c
@@ -1,40 +1,62 @@
-#include<stdio.h>
-int main(){
-	int n,k;
- 	do {
-        printf("Nhap so luong phan tu trong mang (0 < n <= 100): ");
+#include <stdio.h>
+#include <stdlib.h>
+
+void deleteElementArray(int **arr, int *n, int position) {
+    for (int i = position; i < *n - 1; i++) {
+        (*arr)[i] = (*arr)[i + 1];
+    }
+    *arr = (int *)realloc(*arr, (*n - 1) * sizeof(int));
+    (*n)--;
+}
+
+int findPosition(int *arr, int n, int value) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == value) {
+            return i; 
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n, value, position;
+    int *pArray = NULL;
+
+    do {
+        printf("Nhap so phan tu cua mang: ");
         scanf("%d", &n);
         if (n <= 0 || n > 100) {
-            printf("So luong phan tu trong mang phai trong khoang tu 1 den 100.\n");
+            printf("So luong phan tu cua mang nam tu 1 den 100.\n");
         }
     } while (n <= 0 || n > 100);
 
-    int arr[n];
-    printf("Nhap cac phan tu trong mang:\n");
-    for(int i=0;i<n;i++){
-    	printf("phan tu [%d]",i);
-    	scanf("%d",&arr[i]);
-	}
-	printf("Nhap phan tu k can xoa: ");
-    scanf("%d", &k);
-    int i=0;
-	for( i=0;i<n;i++){
-		if(arr[i]==k){
-			for(int j=i;j<n-1;j++){
-				arr[j]=arr[j+1];
-			}
-			n--;
-			break;
-		}
-	}
-	if (i < n) { 
-        printf("Phan tu %d da duoc xoa. Mang sau khi xoa:\n", k);
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-    } else {
-        printf("Khong tim thay phan tu %d trong mang.\n", k);
+    pArray = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        printf("arr[%d] = ", i);
+        scanf("%d", &pArray[i]);
     }
-	return 0;
+
+    printf("Nhap gia tri can xoa: ");
+    scanf("%d", &value);
+
+    position = findPosition(pArray, n, value);
+    if (position == -1) {
+        printf("Gia tri %d khong ton tai trong mang.\n", value);
+    } else {
+
+        while ((position = findPosition(pArray, n, value)) != -1) {
+            deleteElementArray(&pArray, &n, position);
+        }
+        printf("\nMang sau khi xoa gia tri:\n");
+        if (n == 0) {
+            printf("Mang rong.\n");
+        } else {
+            for (int i = 0; i < n; i++) {
+                printf("arr[%d] = %d\n", i, pArray[i]);
+            }
+        }
+    }
+
+    free(pArray); 
+    return 0;
 }
